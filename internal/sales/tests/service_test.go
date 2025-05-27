18a6a21d0dbe139b445de4c840df0fbd773496f7
luuloyola <lucianaloyola30@gmail.com>
2025-05-27 11:14:20 -0300

    intento de test unitario

--- internal/sales/tests/service_test.go
+++ internal/sales/tests/service_test.go
@@ -1,1 +1,28 @@
 package tests
+
+import (
+	"ej_final/internal/sales"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestService_Create_Simple(t *testing.T) {
+	mockHandler := http.NewServeMux()
+	mockServer := httptest.NewServer(mockHandler)
+	defer mockServer.Close()
+
+	s := sales.NewService(sales.NewLocalStorage(), nil, mockServer.URL)
+
+	input := &sales.Sales{
+		UserID: "Pepe",
+		Amount: 1.0,
+	}
+
+	err := s.Create(input)
+
+	require.EqualError(t, err, sales.ErrUserNotFound.Error())
+	require.NotEmpty(t, input.UserID)
+	require.NotEmpty(t, input.Amount)
+}
